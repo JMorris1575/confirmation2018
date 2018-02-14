d6493c4cf1faef300f5168a70f101287dad46386
JMorris1575 <FrJamesMorris@gmail.com>
2018-02-13 21:57:47 -0500

    List of Groups ordered by last name

--- confirmation/mail/views.py
+++ confirmation/mail/views.py
@@ -14,9 +14,9 @@ class EmailView(View):
             group_names = PageMixin.get_group_names(self, request.user)
         else:
             group_names = None
-        supervisors = Group.objects.get(name="Supervisor").user_set.all()
-        team_members = Group.objects.get(name="Team").user_set.all()
-        candidates = Group.objects.get(name="Candidate").user_set.all()
+        supervisors = Group.objects.get(name="Supervisor").user_set.all().order_by('last_name')
+        team_members = Group.objects.get(name="Team").user_set.all().order_by('last_name')
+        candidates = Group.objects.get(name="Candidate").user_set.all().order_by('last_name')
         context = {'group_names': group_names,
                    'supervisors': supervisors,
                    'team_members': team_members,
