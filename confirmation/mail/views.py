92d6de03353c8ec362123eb2ee5c6e50ae886bae
JMorris1575 <FrJamesMorris@gmail.com>
2018-02-16 21:12:32 -0500

    Tag conversion working for e-mails - may have to be html escaped

--- confirmation/mail/views.py
+++ confirmation/mail/views.py
@@ -4,8 +4,33 @@ from django.contrib.auth.models import User, Group
 from django.core.mail import send_mail
 
 from config.mixins import PageMixin
+from config.settings.base import get_secret
 
 # Create your views here.
+
+def convert_tags(subject, message, user):
+    """
+    Converts the following tags in the message or subject line of an e-mail to their equivalent for the given user:
+    [first] = first name
+    [last] = last name
+    [full] = full name
+    [user] = username
+    [pwrd] = password
+    :param subject: string containing the subject line to be sent
+    :param message: string containing the message to be sent
+    :param user: User object containing data for the given user
+    :return: a subject string and message string with all the tags filled in
+    """
+    user_info = {'[first]':user.first_name,
+                 '[last]': user.last_name,
+                 '[full]': user.get_full_name(),
+                 '[user]': user.username,
+                 '[pwrd]': get_secret(user.username.upper())}
+    for tag in user_info.keys():
+        subject = subject.replace(tag, user_info[tag])
+        message = message.replace(tag, user_info[tag])
+
+    return subject, message
 
 
 class EmailView(View):
@@ -28,11 +53,10 @@ class EmailView(View):
     def post(self, request):
         recipients = request.POST.getlist('recipients')
         subject_template = request.POST['subject']
-        subject = subject_template      # until you write a conversion method
         message_template = request.POST['message']
-        message = message_template      # until you write a conversion method
         for recipient in recipients:
             member = User.objects.get(username=recipient)
+            subject, message = convert_tags(subject_template, message_template, member)
             send_mail(subject, message, 'FrJamesMorris@gmail.com', [member.email], fail_silently=False,)
 
         return redirect('send_email')
