586c041d60fce921656e7991a008e02ccfd975d5
JMorris1575 <FrJamesMorris@gmail.com>
2018-09-11 22:20:18 -0400

    Working on responding to acme-challenge

--- confirmation/webfaction_wellknown/views.py
+++ confirmation/webfaction_wellknown/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 
-import os
+import os, sys
 
 # Create your views here.
 
 def data(request, challenge_filename):
-    path_to_file = './webapps/conf18/confirmation/webfaction_wellknown/.well-known/acme-challenge/'
+    # path_to_file = './webapps/conf18/confirmation/webfaction_wellknown/.well-known/acme-challenge/' original
+    print('*** got to views.py data with challenge_filename = ', challenge_filename, file=sys.stderr, flush=True)
+    path_to_file = '.well-known/acme-challenge/'
     f = open(path_to_file + challenge_filename, 'r')
     data = f.read()
     f.close()
